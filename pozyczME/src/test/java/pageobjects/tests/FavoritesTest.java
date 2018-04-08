50264d9f0d64ccccb7bb978d8b6f95ad83d033be
Agata Pankowska <pa.agatka@gmail.com>
2018-04-08 15:50:16 +0200

    some changes in pom.xml

--- pozyczME/src/test/java/pageobjects/tests/FavoritesTest.java
+++ pozyczME/src/test/java/pageobjects/tests/FavoritesTest.java
@@ -1,20 +1,13 @@
 package pageobjects.tests;
 
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import pageobjects.BaseClassTest;
-import pageobjects.User;
-import pageobjects.Waits;
-import pageobjects.pages.MainPage;
-import pageobjects.pages.MyProfilePage;
-import pageobjects.pages.SignInPage;
-import pageobjects.pages.SignUpPage;
 
-import java.util.concurrent.TimeUnit;
+import pageobjects.BaseClassTest;
+
+import pageobjects.pages.MyProfilePage;
+
 
 import static org.junit.Assert.*;
 
