a1bd611437dbf48f944e65e921b17d78e2a3f659
waldemar <waldmar.mozola@onet.pl>
2018-04-13 12:33:30 +0200

    Create packege testWatcher and create method TakeScreenshot

--- pozyczME/src/test/java/pageobjects/tests/SearchForTest.java
+++ pozyczME/src/test/java/pageobjects/tests/SearchForTest.java
@@ -62,67 +62,7 @@ public class SearchForTest extends BaseClassTest {
         searchForPage.setSearchField("Scrabble");
         assertTrue("SearchField is not displayed", searchForPage.isSearchFieldVisible());
     }
-    @Test
 
-    public void testScreenShot() throws Exception{
-
-        WebDriver driver ;
-        System.setProperty("webdriver.chrome.driver","/home/waldemar/Pobrane/Screeshoot/pozyczME/src/test/resources/chromedriver");
-        driver = new ChromeDriver();
-
-        //goto url
-
-        driver.get("http://app.pinapple.jdqz1.is-academy.pl/");
-
-        //Call take screenshot function
-
-        this.takeSnapShot(driver, "/home/waldemar/Pobrane/test.png") ;
-
-    }
-
-        public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
-
-        //Convert web driver object to TakeScreenshot
-
-        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
-
-        //Call getScreenshotAs method to create image file
-
-        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
-
-        //Move image file to new destination
-
-        File DestFile = new File(fileWithPath);
-
-        //Copy file at destination
-
-        FileUtils.copyFile(SrcFile, DestFile);
-
-    }
-
-    public static class WatchmanTest {
-        private static String watchedLog;
-
-        @Rule
-        public TestWatcher watchman = new TestWatcher() {
-            @Override
-            protected void failed(Throwable e, Description description) {
-                watchedLog += description + "\n";
-            }
-
-            @Override
-            protected void succeeded(Description description) {
-                watchedLog += description + " " + "success!\n";
-            }
-        };
-
-        @Test
-        public void fails() {
-            fail();
-        }
-
-        @Test
-        public void succeeds() {
-        }
-    }
+    @Rule
+    public SearchForTest watchman = new SearchForTest();
 }
